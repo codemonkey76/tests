bb8d5a7dc529a07d2714fcebae3e57ddb154e745
Shane Poppleton <shane@bjja.com.au>
2017-07-06 21:33:01 +1000

    Added UserImport script, setup email services

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'belt_id', 'password',
+        'name', 'email', 'belt_id', 'password', 'active', 'can_promote', 'instructor', 
     ];
 
     /**
